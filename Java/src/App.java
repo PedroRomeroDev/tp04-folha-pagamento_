6f972b01c9c6ca0ec97c8b5327c909984b5ac7e8
Hugo <hugoeduardogd@gmail.com>
2026-09-03 18:37:26 -0300

    Calculo das horas extras

--- Java/src/App.java
+++ Java/src/App.java
@@ -2,19 +2,24 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
+
     
 
-        public static double calcularImposto(double salarioBruto, double horaextra) {
+        public static double calcularImposto(double salarioBruto) {
         double inss = 14.0;
         double ir = 27.0;
         double desconto = salarioBruto * ((inss / 100.0) + (ir / 100.0));
         double salarioLiquido = salarioBruto - desconto;
-        
-        if (horaextra > 0) {
-            salarioLiquido += 50.0;
         }
-        
         return salarioLiquido;
         }
+
+        public horaextra(double horaextra){
+
+            if (horaextra > 0) {
+                salarioLiquido += 50.0;
+            }
+
+        }
     }
-}
+
