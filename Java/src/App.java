16d6812952bf0d4b775c23bc511f67b4a50067c8
Hugo <hugoeduardogd@gmail.com>
2026-09-03 18:28:41 -0300

    Funções para hora extra

--- Java/src/App.java
+++ Java/src/App.java
@@ -4,18 +4,17 @@ public class App {
     public static void main(String[] args) throws Exception {
     
 
-        public imposto(){
-
-            double salarioLiquido = salarioBruto * (inss / 100,00 + ir / 100,00);
-
-            double inss = 14;
-            double ir = 27;
-
-            if (horaextra > 0){
-
-                salarioLiquido + 50; 
-
-            }
+        public static double calcularImposto(double salarioBruto, double horaextra) {
+        double inss = 14.0;
+        double ir = 27.0;
+        double desconto = salarioBruto * ((inss / 100.0) + (ir / 100.0));
+        double salarioLiquido = salarioBruto - desconto;
+        
+        if (horaextra > 0) {
+            salarioLiquido += 50.0;
+        }
+        
+        return salarioLiquido;
         }
     }
 }
