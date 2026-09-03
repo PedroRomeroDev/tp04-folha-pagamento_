d906ecd8ed57ab07da1c39b6603639d854d22dfd
Pedro Romero <romeropedrao2007@gmail.com>
2026-09-03 18:36:20 -0300

    Recebendo Horas Extras do Funcionario

--- Java/src/App.java
+++ Java/src/App.java
@@ -11,6 +11,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
         double salarioBruto;
+        int horasExtras;
         header();
 
         System.out.print("NOME DO FUNCIONARIO: ");
@@ -20,6 +21,11 @@ public class App {
             System.out.print("SALÁRIO BRUTO: ");
             salarioBruto = s.nextDouble();
         } while (salarioBruto < 0.00);
+
+        do {
+            System.out.print("HORAS EXTRAS: ");
+            horasExtras = s.nextInt();
+        } while(horasExtras < 0);
 
     }
 }
