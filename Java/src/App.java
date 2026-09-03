575671ad3e584658da981d87c86aa80930c8daa9
Pedro Romero <romeropedrao2007@gmail.com>
2026-09-03 18:33:20 -0300

    Recebendo Salario Bruto Funcionario

--- Java/src/App.java
+++ Java/src/App.java
@@ -9,8 +9,17 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-
+        Scanner s = new Scanner(System.in);
+        double salarioBruto;
         header();
-        
+
+        System.out.print("NOME DO FUNCIONARIO: ");
+        String nome = s.next();
+
+        do {
+            System.out.print("SALÁRIO BRUTO: ");
+            salarioBruto = s.nextDouble();
+        } while (salarioBruto < 0.00);
+
     }
 }
